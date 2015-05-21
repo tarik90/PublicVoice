09fcb9a3dacebd03b6521539c4cfaabfe32b6ca1
tarik90 <taralam@my.lonestar.edu>
2015-05-20 20:35:43 -0500

    no motivation

---
+++ index.js
@@ -0,0 +1,5 @@
+
+var screen_style = document.getElementById("box");
+screen_style.style.width = screen.width + "px";
+screen_style.style.height = screen.height + "px";
+screen_style.style.backgroundColor = "black";
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <title>Public Voice</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="index.css">
+      <script type="text/javascript" src="index.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div id="box">
+      <script >
+        var screen_style = document.getElementById("box");
+        screen_style.style.width = (screen.width -19) + "px";
+        screen_style.style.height = (screen.height - 111) + "px";
+        screen_style.style.backgroundColor = "black";
+      </script>
+      dfgdfg
+      <!--
+      <div id="head">
+        cdfsdfsdfsd
+      </div>
+      
+      <div id="midpage">
+
+        <div id="subject">
+          hgjghjghj
+        </div>
+
+        <div id="poll">
+          hjkhjk
+        </div>
+
+        <div id="news">
+          ghjghjghj
+        </div>
+
+      </div>
+    -->
+    </div>
+
+
+
+     
+      
+  </body>
+</html>
