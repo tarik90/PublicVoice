832527cdee8658cec2e0ebcc54f5136826de67b9
tarik90 <taralam@my.lonestar.edu>
2015-05-24 02:29:29 -0500

    some update

--- index.php
+++ index.php
@@ -13,15 +13,6 @@
 
   </head>
   <body>
-    <p>Demonstrate fadeToggle() with different speed parameters.</p>
-
-<button>Click to fade in/out boxes</button><br><br>
-
-<div id="div1" style="width:80px;height:80px;background-color:red;"></div>
-<br>
-<div id="div2" style="width:80px;height:80px;background-color:green;"></div>
-<br>
-<div id="div3" style="width:80px;height:80px;background-color:blue;"></div>
     <div id="box">
       <script >
         var screen_style = document.getElementById("box");
@@ -35,17 +26,19 @@
       <div id="head">
         <script >
           var head_style = document.getElementById("head");
-          head_style.style.maxWidth = (screen.width -18) + "px";
+          head_style.style.width = (screen.width -18) + "px";
           head_style.style.height = (screen.height - 630) + "px";
           head_style.style.backgroundColor = "#335C33";
 
         </script>
 
-        <div id="website_logo">
-          <span>Public Voice</span>
-        </div>
-
         <div id="navigation">
+          <script >
+            var naviga_tion = document.getElementById("navigation");
+            naviga_tion.style.width = (screen.width -18) + "px";
+          </script>
+ <!--           
+
           <nav class="navbar navbar-inverse">
             <div class="container-fluid">
               <div class="navbar-header">
@@ -68,30 +61,87 @@
               </div>
             </div>
           </nav>
+
+-->
+
+          <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>                        
+                </button>
+                <a class="navbar-brand" href="#"><span id="public">Public</span>
+                                                  <span id="voice">Voice</span></a>
+              </div>
+              <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                  <li class="active"><a id="home" href="#">Home</a></li>
+                  <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Page 1-1</a></li>
+                      <li><a href="#">Page 1-2</a></li>
+                      <li><a href="#">Page 1-3</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Page 2</a></li>
+                  <li><a href="#">Page 3</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                  <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                  <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        
         </div>
 
-        <p>sdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldf</p>
+
       </div>
     
       <div id="midpage">
         <script >
           var mid_page = document.getElementById("midpage");
           mid_page.style.width = (screen.width -18) + "px";
-          mid_page.style.height = (screen.height - 400) + "px";
+          mid_page.style.height = (screen.height - 350) + "px";
           mid_page.style.backgroundColor = "green";
         </script>
-<!--
-        <div id="subject">
-          hgjghjghj
-        </div>
 
+        <div id="subjects">
+          <script >
+          var sub_jects = document.getElementById("subjects");
+          sub_jects.style.width = (screen.width -1200) + "px";
+          sub_jects.style.height = (screen.height - 350) + "px";
+          sub_jects.style.backgroundColor = "blue";
+        </script>
+
+        </div>
+  
         <div id="poll">
-          hjkhjk
+          <script >
+          var poll_box = document.getElementById("poll");
+          poll_box.style.width = (screen.width -600) + "px";
+          poll_box.style.height = (screen.height - 350) + "px";
+          poll_box.style.backgroundColor = "yellow";
+        </script>
+                  <p>sdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldf</p>
+
         </div>
 
         <div id="news">
-          ghjghjghj
+          <script >
+          var news_bos = document.getElementById("news");
+          news_bos.style.width = (screen.width -950) + "px";
+          news_bos.style.height = (screen.height - 350) + "px";
+          news_bos.style.backgroundColor = "black";
+        </script>
+                  <p>sdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldf</p>
+
         </div>
+
 -->
       </div>
 
