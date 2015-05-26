52b99d4cbf3041c7f85fb22515e0e520385b9c5e
tarik90 <taralam@my.lonestar.edu>
2015-05-26 02:20:42 -0500

    lots of change again.

---
+++ index.js
@@ -0,0 +1,10 @@
+$(function(){
+	var repeat_counter = 30;
+	while( repeat_counter != 0){
+		$("#newsB").fadeToggle(1000);
+		repeat_counter--;
+		// if (repeat_counter == 0) {
+		// 	repeat_counter = 5;
+		// }
+	}
+})
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
       <title>Public Voice</title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
       <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
       <link rel="stylesheet" type="text/css" href="index.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
@@ -27,7 +28,7 @@
         <script>
           var head_style = document.getElementById("head");
           head_style.style.width = (screen.width -18) + "px";
-          head_style.style.height = (screen.height - 630) + "px";
+          head_style.style.height = (screen.height - 600) + "px";
           head_style.style.backgroundColor = "#335C33";
 
         </script>
@@ -77,7 +78,7 @@
               </div>
               <div class="collapse navbar-collapse" id="myNavbar">
                 <ul class="nav navbar-nav">
-                  <li class="active"><a id="home" href="#">Home</a></li>
+                  <li class="active"><a id="home" href="#">HOME</a></li>
                   <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
                     <ul class="dropdown-menu">
@@ -113,7 +114,7 @@
         <div id="subjects">
           <script>
           var sub_jects = document.getElementById("subjects");
-          sub_jects.style.width = (screen.width -1150) + "px";
+          sub_jects.style.width = (screen.width -1060) + "px";
           sub_jects.style.height = (screen.height - 350) + "px";
           sub_jects.style.backgroundColor = "blue";
           sub_jects.style.transition = "all 1s";
@@ -123,150 +124,180 @@
             <script >
               var on_e = document.getElementById("one");
               // on_e.style.width = (screen.width -1200) + "px";
-              on_e.style.height = (screen.height - 740) + "px";
-              on_e.style.backgroundColor = "red";
-               on_e.style.transition = "all 1s";
+              on_e.style.height = (screen.height - 740.3) + "px";
+              on_e.style.backgroundColor = "#202020";
+              on_e.style.lineHeight = on_e.style.height;
+              on_e.style.transition = "all 1s";
             </script>
+            FOOD
           </div>
 
           <div id="two">
             <script>
               var tw_o = document.getElementById("two");
               // tw_o.style.width = (screen.width -1200) + "px";
-              tw_o.style.height = (screen.height - 740) + "px";
-              tw_o.style.backgroundColor = "black";
+              tw_o.style.height = (screen.height - 740.3) + "px";
+              tw_o.style.backgroundColor = "#303030";
+              tw_o.style.lineHeight = tw_o.style.height;
               tw_o.style.transition = "all 1s";
             </script>
+            POLITICS
           </div>
 
           <div id="three">
             <script>
               var thr_ee = document.getElementById("three");
               // thr_ee.style.width = (screen.width -1200) + "px";
-              thr_ee.style.height = (screen.height - 740) + "px";
-              thr_ee.style.backgroundColor = "red";
+              thr_ee.style.height = (screen.height - 740.3) + "px";
+              thr_ee.style.backgroundColor = "#202020";
+              thr_ee.style.lineHeight = thr_ee.style.height;
               thr_ee.style.transition = "all 1s";
             </script>
+            RELIGION
           </div>
 
           <div id="four">
             <script>
               var thr_ee = document.getElementById("four");
               // thr_ee.style.width = (screen.width -1200) + "px";
-              thr_ee.style.height = (screen.height - 740) + "px";
-              thr_ee.style.backgroundColor = "black";
+              thr_ee.style.height = (screen.height - 740.3) + "px";
+              thr_ee.style.backgroundColor = "#303030";
+              thr_ee.style.lineHeight = thr_ee.style.height;
               thr_ee.style.transition = "all 1s";
             </script>
+            AUTO
           </div>
 
           <div id="five">
             <script>
               var thr_ee = document.getElementById("five");
               // thr_ee.style.width = (screen.width -1200) + "px";
-              thr_ee.style.height = (screen.height - 740) + "px";
-              thr_ee.style.backgroundColor = "red";
+              thr_ee.style.height = (screen.height - 740.3) + "px";
+              thr_ee.style.backgroundColor = "#202020";
+              thr_ee.style.lineHeight = thr_ee.style.height;
               thr_ee.style.transition = "all 1s";
             </script>
+            EDUCATION
           </div>
 
           <div id="six">
             <script >
               var on_e = document.getElementById("six");
               // on_e.style.width = (screen.width -1200) + "px";
-              on_e.style.height = (screen.height - 740) + "px";
-              on_e.style.backgroundColor = "black";
+              on_e.style.height = (screen.height - 740.3) + "px";
+              on_e.style.backgroundColor = "#303030";
+              on_e.style.lineHeight = on_e.style.height;
               on_e.style.transition = "all 1s";
             </script>
+            SOCIAL
           </div>
 
           <div id="seven">
             <script>
               var tw_o = document.getElementById("seven");
               // tw_o.style.width = (screen.width -1200) + "px";
-              tw_o.style.height = (screen.height - 740) + "px";
-              tw_o.style.backgroundColor = "red";
+              tw_o.style.height = (screen.height - 740.3) + "px";
+              tw_o.style.backgroundColor = "#202020";
+              tw_o.style.lineHeight = tw_o.style.height;
               tw_o.style.transition = "all 1s";
             </script>
+            HEALTH & BEAUTY
           </div>
 
           <div id="eight">
             <script>
               var thr_ee = document.getElementById("eight");
               // thr_ee.style.width = (screen.width -1200) + "px";
-              thr_ee.style.height = (screen.height - 740) + "px";
-              thr_ee.style.backgroundColor = "black";
+              thr_ee.style.height = (screen.height - 740.3) + "px";
+              thr_ee.style.backgroundColor = "#303030";
+              thr_ee.style.lineHeight = thr_ee.style.height;
               thr_ee.style.transition = "all 1s";
             </script>
+            MUSIC & ENTERTAINMENT
           </div>
 
           <div id="nine">
             <script>
               var tw_o = document.getElementById("nine");
               // tw_o.style.width = (screen.width -1200) + "px";
-              tw_o.style.height = (screen.height - 740) + "px";
-              tw_o.style.backgroundColor = "red";
+              tw_o.style.height = (screen.height - 740.3) + "px";
+              tw_o.style.backgroundColor = "#202020";
+              tw_o.style.lineHeight = tw_o.style.height;
               tw_o.style.transition = "all 1s";
             </script>
+            ENVIRONMENT & NATURE
           </div>
 
           <div id="ten">
             <script>
               var thr_ee = document.getElementById("ten");
               // thr_ee.style.width = (screen.width -1200) + "px";
-              thr_ee.style.height = (screen.height - 740) + "px";
-              thr_ee.style.backgroundColor = "black";
+              thr_ee.style.height = (screen.height - 740.3) + "px";
+              thr_ee.style.backgroundColor = "#303030";
+              thr_ee.style.lineHeight = thr_ee.style.height;
               thr_ee.style.transition = "all 1s";
             </script>
+            TECH & ENGINEERING
           </div>
 
           <div id="eleven">
             <script>
               var tw_o = document.getElementById("eleven");
               // tw_o.style.width = (screen.width -1200) + "px";
-              tw_o.style.height = (screen.height - 740) + "px";
-              tw_o.style.backgroundColor = "red";
+              tw_o.style.height = (screen.height - 740.3) + "px";
+              tw_o.style.backgroundColor = "#202020";
+              tw_o.style.lineHeight = tw_o.style.height;
               tw_o.style.transition = "all 1s";
             </script>
+            BEAUTY
           </div>
 
           <div id="twelve">
             <script>
               var thr_ee = document.getElementById("twelve");
               // thr_ee.style.width = (screen.width -1200) + "px";
-              thr_ee.style.height = (screen.height - 740) + "px";
-              thr_ee.style.backgroundColor = "black";
+              thr_ee.style.height = (screen.height - 740.3) + "px";
+              thr_ee.style.backgroundColor = "#303030";
+              thr_ee.style.lineHeight = thr_ee.style.height;
               thr_ee.style.transition = "all 1s";
             </script>
+            SPORTS
           </div>
 
           <div id="thirteen">
             <script>
               var thr_ee = document.getElementById("thirteen");
               // thr_ee.style.width = (screen.width -1200) + "px";
-              thr_ee.style.height = (screen.height - 740) + "px";
-              thr_ee.style.backgroundColor = "red";
+              thr_ee.style.height = (screen.height - 740.3) + "px";
+              thr_ee.style.backgroundColor = "#202020";
+              thr_ee.style.lineHeight = thr_ee.style.height;
               thr_ee.style.transition = "all 1s";
             </script>
+            PERSONAL
           </div>
 
           <div id="fourteen">
             <script>
               var tw_o = document.getElementById("fourteen");
               // tw_o.style.width = (screen.width -1200) + "px";
-              tw_o.style.height = (screen.height - 740) + "px";
-              tw_o.style.backgroundColor = "black";
+              tw_o.style.height = (screen.height - 740.3) + "px";
+              tw_o.style.backgroundColor = "#303030";
+              tw_o.style.lineHeight = tw_o.style.height;
               tw_o.style.transition = "all 1s";
             </script>
+            FASHION
           </div>
 
           <div id="fifteen">
             <script>
               var thr_ee = document.getElementById("fifteen");
               // thr_ee.style.width = (screen.width -1200) + "px";
-              thr_ee.style.height = (screen.height - 740) + "px";
-              thr_ee.style.backgroundColor = "red";
+              thr_ee.style.height = (screen.height - 740.3) + "px";
+              thr_ee.style.backgroundColor = "#202020";
+              thr_ee.style.lineHeight = thr_ee.style.height;
               thr_ee.style.transition = "all 1s";
             </script>
+            MORE+ 
           </div>
 
 
@@ -276,8 +307,7 @@
         <div id="poll">
           <script>
           var poll_box = document.getElementById("poll");
-          var sub_jects = document.getElementById("subjects");
-          poll_box.style.width = (screen.width -600) + "px";
+          poll_box.style.width = (screen.width -660) + "px";
           poll_box.style.height = (screen.height - 350) + "px";
           poll_box.style.backgroundColor = "yellow";
           poll_box.style.transition = "all 1s";
@@ -288,12 +318,62 @@
 
         <div id="news">
           <script>
-          var news_box = document.getElementById("news");
-          news_box.style.width = (screen.width -1090) + "px";
-          news_box.style.height = (screen.height - 350) + "px";
-          news_box.style.backgroundColor = "black";
-        </script>
-                  <p>sdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldfsdfsdlfsdflkjsdlfkjsldf</p>
+            var news_box = document.getElementById("news");
+            news_box.style.width = (screen.width -1090) + "px";
+            news_box.style.height = (screen.height - 350) + "px";
+            news_box.style.backgroundColor = "#303030";
+          </script>
+
+          <div id="newsHead">
+            <script>
+              var news_head = document.getElementById("newsHead");
+              news_head.style.width = (screen.width -1090) + "px";
+              news_head.style.height = (screen.height - 700) + "px";
+              news_head.style.backgroundColor = "#335C33";
+              news_head.style.lineHeight = news_head.style.height;
+            </script>
+            <p id="newsB">NEWS BOX<p/>
+          </div>
+
+          <div id="cnn">
+            <script>
+              var news_cnn = document.getElementById("cnn");
+              news_cnn.style.width = (screen.width -1090) + "px";
+              news_cnn.style.backgroundColor = "#202020";
+              news_cnn.style.lineHeight = news_cnn.style.height;
+            </script>
+            CNN
+          </div>
+
+          <div id="bbc">
+            <script>
+              var news_bbc = document.getElementById("bbc");
+              news_bbc.style.width = (screen.width -1090) + "px";
+              news_bbc.style.backgroundColor = "#303030";
+              news_bbc.style.lineHeight = news_bbc.style.height;
+            </script>
+            BBC
+          </div>
+
+          <div id="rt">
+            <script>
+              var news_rt = document.getElementById("rt");
+              news_rt.style.width = (screen.width -1090) + "px";
+              news_rt.style.backgroundColor = "#202020";
+              news_rt.style.lineHeight = news_rt.style.height;
+            </script>
+            RT
+          </div>
+
+          <div id="forbes">
+            <script>
+              var news_forbes = document.getElementById("forbes");
+              news_forbes.style.width = (screen.width -1090) + "px";
+              news_forbes.style.backgroundColor = "#303030";
+              news_forbes.style.lineHeight = news_forbes.style.height;
+            </script>
+            FORBES
+          </div>
 
         </div>
 
