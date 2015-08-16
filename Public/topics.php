3a576ee9589772a029f33007915049ecece77a3e
tarik90 <taralam@my.lonestar.edu>
2015-08-16 10:27:58 +0600

    CHANGED TO CAPITAL LETTER

--- Public/topics.php
+++ Public/topics.php
@@ -10,18 +10,18 @@
         </div>
 
         <div id="eachBox">
-          <a href="subject.php?tag=techEngineering"><div id="topic">Tech & Engineering</div></a>
-          <a href="subject.php?tag=environmentNature"><div id="topic">Environment & Nature</div></a>
-          <a href="subject.php?tag=healthBeauty"><div id="topic">Health & Beauty</div></a>
-          <a href="subject.php?tag=musicEntertainment"><div id="topic">Music & Entertainment</div></a>
-          <a href="subject.php?tag=social"><div id="topic">Social</div></a>
+          <a href="subject.php?tag=techEngineering"><div id="topic">TECH & ENGINEERING</div></a>
+          <a href="subject.php?tag=environmentNature"><div id="topic">ENVIRONMENT & NATURE</div></a>
+          <a href="subject.php?tag=healthBeauty"><div id="topic">HEALTH & BEAUTY</div></a>
+          <a href="subject.php?tag=musicEntertainment"><div id="topic">MUSIC & ENTERTAINMENT</div></a>
+          <a href="subject.php?tag=social"><div id="topic">SOCIAL</div></a>
         </div>
           
         <div id="eachBox">
-          <a href="subject.php?tag=world"><div id="topic">World</div></a>
-          <a href="subject.php?tag=sports"><div id="topic">Sports</div></a>
-          <a href="subject.php?tag=personal"><div id="topic">Personal</div></a>
-          <a href="subject.php?tag=fashion"><div id="topic">Fashion</div></a>
+          <a href="subject.php?tag=world"><div id="topic">WORLD</div></a>
+          <a href="subject.php?tag=sports"><div id="topic">SPORTS</div></a>
+          <a href="subject.php?tag=personal"><div id="topic">PERSONAL</div></a>
+          <a href="subject.php?tag=fashion"><div id="topic">FASHION</div></a>
           <a href="subject.php?tag=wtfs"><div id="topic">WTF'S</div></a>
         </div>
           
